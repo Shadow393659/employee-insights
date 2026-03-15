f96bc3f5d5c91bf527b6f96ec8535f9ee1f0172c
Shadow393659 <abdulrahmanr861@gmail.com>
2026-03-15 21:42:06 +0530

    Add Navbar and Sidebar to Layout component
    Refactor Layout component to include Navbar and Sidebar.

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -7,7 +7,32 @@ function Layout({ children }) {
       <Sidebar />
 
       <div className="flex-1">
+        <Navbar />import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
+
+function Layout({ children }) {
+  return (
+    <div className="min-h-screen flex flex-col md:flex-row">
+
+      {/* Sidebar */}
+      <div className="w-full md:w-64">
+        <Sidebar />
+      </div>
+
+      {/* Main Content */}
+      <div className="flex-1 flex flex-col">
         <Navbar />
+
+        <div className="p-4 md:p-6 bg-slate-100 min-h-screen overflow-x-auto">
+          {children}
+        </div>
+      </div>
+
+    </div>
+  );
+}
+
+export default Layout;
 
         <div className="p-6 bg-slate-100 min-h-screen">{children}</div>
       </div>
