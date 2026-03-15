50231ca5930599098a84b952c9eb9e824262ccee
Shadow393659 <abdulrahmanr861@gmail.com>
2026-03-15 21:46:58 +0530

    Fix import statements and layout structure

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,15 +3,6 @@ import Sidebar from "./Sidebar";
 
 function Layout({ children }) {
   return (
-    <div className="flex">
-      <Sidebar />
-
-      <div className="flex-1">
-        <Navbar />import Navbar from "./Navbar";
-import Sidebar from "./Sidebar";
-
-function Layout({ children }) {
-  return (
     <div className="min-h-screen flex flex-col md:flex-row">
 
       {/* Sidebar */}
