59f18bd2614a4683c8f28955c0bba4bad682ea21
Shadow393659 <abdulrahmanr861@gmail.com>
2026-03-15 22:09:40 +0530

    Fix mapping of visibleData to EmployeeRow component

--- src/components/VirtualTable.jsx
+++ src/components/VirtualTable.jsx
@@ -13,8 +13,8 @@ function VirtualTable({ data }) {
     <div className="h-[500px] overflow-y-auto border" onScroll={handleScroll}>
       <div style={{ height: totalHeight, position: "relative" }}>
         <div style={{ transform: `translateY(${offsetY}px)` }}>
-          {visibleData.map((index,emp) => (
-            <EmployeeRow key={index} employee={emp} />
+          {visibleData.map((emp) => (
+            <EmployeeRow key={emp.id} employee={emp} />
           ))}
         </div>
       </div>
