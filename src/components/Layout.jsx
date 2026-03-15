73515cc2826b250c8a54b59741d2f598e314848b
Shadow393659 <abdulrahmanr861@gmail.com>
2026-03-15 21:48:54 +0530

    Update Layout component structure and styles
    Refactor Layout component for improved structure and styling.

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,13 +3,19 @@ import Sidebar from "./Sidebar";
 
 function Layout({ children }) {
   return (
-    <div className="flex">
-      <Sidebar />
+    <div className="min-h-screen flex flex-col md:flex-row">
+      {/* Sidebar */}
+      <div className="w-full md:w-64">
+        <Sidebar />
+      </div>
 
-      <div className="flex-1">
+      {/* Main Content */}
+      <div className="flex-1 flex flex-col">
         <Navbar />
 
-        <div className="p-6 bg-slate-100 min-h-screen">{children}</div>
+        <div className="p-4 md:p-6 bg-slate-100 min-h-screen overflow-x-auto">
+          {children}
+        </div>
       </div>
     </div>
   );
