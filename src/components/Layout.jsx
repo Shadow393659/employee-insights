ae97c2ab93e415d597e6251e09adcebbdc1516db
Shadow393659 <abdulrahmanr861@gmail.com>
2026-03-15 21:47:25 +0530

    Refactor Layout component structure

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,27 +3,11 @@ import Sidebar from "./Sidebar";
 
 function Layout({ children }) {
   return (
-    <div className="min-h-screen flex flex-col md:flex-row">
+    <div className="flex">
+      <Sidebar />
 
-      {/* Sidebar */}
-      <div className="w-full md:w-64">
-        <Sidebar />
-      </div>
-
-      {/* Main Content */}
-      <div className="flex-1 flex flex-col">
+      <div className="flex-1">
         <Navbar />
-
-        <div className="p-4 md:p-6 bg-slate-100 min-h-screen overflow-x-auto">
-          {children}
-        </div>
-      </div>
-
-    </div>
-  );
-}
-
-export default Layout;
 
         <div className="p-6 bg-slate-100 min-h-screen">{children}</div>
       </div>
